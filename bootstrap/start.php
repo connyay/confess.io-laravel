db7a6b64e9890de87b90f34e863658a45b61c322
Connor Hindley <conn.hindley@gmail.com>
2014-04-15 19:41:05 -0400

    add envs

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+	'local' => array('confess-dev'),
+	'production ' => array('confess-io'),
 
 ));
 
