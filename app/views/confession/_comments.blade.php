441400be7008f2fa3a7bdff560d2111da0f39157
Connor Hindley <conn.hindley@gmail.com>
2014-04-15 00:22:23 -0400

    update comment list

--- app/views/confession/_comments.blade.php
+++ app/views/confession/_comments.blade.php
@@ -1,10 +1,15 @@
-<ol class="rectangle-list">
+<ul class="media-list">
+<? $count = 0; ?>
 @foreach ($comments as $comment)
-<li class="pure-u-1-2">
-	<div>
-		<p>{{ $comment->content() }}</p>
-		<h5><small><em>{{{ $comment->date() }}}</em></small></h5>
-	</div>
+<li class="media">
+	<hr>
+	<div class="pull-left">
+      <p><b>{{++$count }}</b><p>
+    </div>
+	<div class="media-body">
+      <h6 class="media-heading"><em>{{ $comment->date() }}</em></h6>
+      <p>{{ $comment->content() }}</p>
+    </div>
 </li>
 @endforeach
-</ol>
+</ul>
