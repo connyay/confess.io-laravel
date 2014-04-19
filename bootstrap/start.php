30e67d17ee1c6d1542622f1bb0aae0c62fa10d79
Connor Hindley <conn.hindley@gmail.com>
2014-04-19 11:10:17 -0400

    upgrade vagrant & puphet

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('confess-dev'),
+	'local' => array('vagrant'),
 	'production' => array('confess-io'),
 
 ));
